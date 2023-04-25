09def297ee6edda0048447721410a3a1c0d65cf1
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-04-25 15:48:15 +0800

    checking for even integers

--- Integers squared and cube.py
+++ Integers squared and cube.py
@@ -1,8 +1,13 @@
 # read external file
 with open('integers.txt', 'r') as main_file, open('double.txt', 'w') as squared_file, open('triple.txt', 'w') as cubic_file:
-# check for even integers
-# square the even integers
-# create separate text file for squared integers
-# check for odd integers
-# cube the odd integers
-# create separate text file for cubic integers
+    for line in main_file:
+        integers = int(line)
+        # check for even integers
+        if integers % 2 == 0:
+            even_nmbrs = str(integers)
+            # square the even integers
+            
+            # create separate text file for squared integers
+            # check for odd integers
+            # cube the odd integers
+            # create separate text file for cubic integers
