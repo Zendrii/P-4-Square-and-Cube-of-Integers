f466c9526a9221fd2b4501ca8760e58991024675
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-04-25 15:39:31 +0800

    pseudocode

---
+++ Integers squared and cube.py
@@ -0,0 +1,7 @@
+# read external file
+# check for even integers
+# square the even integers
+# create separate text file for squared integers
+# check for odd integers
+# cube the odd integers
+# create separate text file for cubic integers
